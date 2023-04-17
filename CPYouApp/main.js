05a06c658a4d6a8fdef7b59e4012d8f00b657255
nwhayes7 <nwhayes7@gmail.com>
2023-04-17 18:18:36 -0400

    added task list front end

--- CPYouApp/main.js
+++ CPYouApp/main.js
@@ -6,6 +6,92 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
+// Add Stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material) // create new star mesh
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)) // create random x, y, z coordinates
+  star.position.set(x, y, z) // set star position
+  scene.add(star) // add star to scene
+}
+
+function generateDay(weekObj) {
+  let x = 3
+  while (x > -4) {
+    const geometry = new THREE.BoxGeometry(5, 1, 5)
+    const material = new THREE.MeshStandardMaterial({ color: 0xc06c84 })
+    const cube = new THREE.Mesh(geometry, material) // create new cube mesh
+    cube.position.set(x*7, -.5, 0) // set cube position
+
+    loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+      const textGeometry = new TextGeometry('June1\nI love pandas', {
+        font: font,
+        size: .4,
+        height: .04,
+        curveSegments: 2
+      })
+      const textMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFFF }) //F8B195
+      const text = new THREE.Mesh(textGeometry, textMaterial)
+      text.position.set(-2.1,.6,-1.8)
+      text.rotateX(-1.6)
+      cube.add(text)
+  })
+    weekObj.add(cube) // add cube to scene
+    x--
+  }
+}
+
+function generateWeeks() {
+  let weeks  = []
+  let z = 1
+  while (z > -3) {
+    const week = new THREE.Object3D()
+    generateDay(week)
+    week.position.set(0, 0, z * 7 + 3.5)
+    weeks.push(week)
+    scene.add(week)
+    z--
+  }
+  return weeks
+} 
+
+function generateTaskList() {
+  let x = 0 // initialize x
+  while (x < 10 || x == 10) { // loop through tsk list up to ten tasks
+    const geometry = new THREE.BoxGeometry(5, 1, 5)
+    const material = new THREE.MeshStandardMaterial({ color: 0xF8B195  })
+    const task = new THREE.Mesh(geometry, material) // create new cube mesh
+    task.position.set(-21 + x*7, -.5, 23) // set cube position
+    
+    loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+      const textGeometry = new TextGeometry('June1\nI love pandas', {
+        font: font,
+        size: .4,
+        height: .04,
+        curveSegments: 2
+      })
+      const textMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFFF }) //F8B195
+      const text = new THREE.Mesh(textGeometry, textMaterial)
+      text.position.set(-2.1,.6,-1.8)
+      text.rotateX(-1.6)
+      task.add(text)
+  })
+    x++
+
+    scene.add(task) // add cube to scene
+  }
+}
+
+function animate() {
+  requestAnimationFrame( animate ) // ask browser to perform animation
+
+  controls.update() // update camera position
+  pointLight.position.set(camera.position.x, camera.position.y, camera.position.z) // move light with camera
+  renderer.render(scene, camera)  // render scene with camera
+}
+
 // Create Scene and Camera to be Rendered
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -32,71 +118,14 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper ,gridHelper)
 
-// Add Stars
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
-  const star = new THREE.Mesh(geometry, material) // create new star mesh
-
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)) // create random x, y, z coordinates
-  star.position.set(x, y, z) // set star position
-  scene.add(star) // add star to scene
-}
-Array(100).fill().forEach(addStar) // add 100 stars to scene
-
+// Add Background Color
 scene.background = new THREE.Color(0x355c7d)
 
-function generateDays(week, fontLoader) {
-  let x = 3
-  while (x > -4) {
-    const geometry = new THREE.BoxGeometry(5, 5, 5)
-    const material = new THREE.MeshStandardMaterial({ color: 0xc06c84 })
-    const cube = new THREE.Mesh(geometry, material) // create new cube mesh
-    cube.position.set(x*7, -2.5, 0) // set cube position
-
-  loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
-    const textGeometry = new TextGeometry('June1\nI love pandas', {
-      font: font,
-      size: .4,
-      height: .04,
-      curveSegments: 2
-    })
-    const textMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFFF }) //F8B195
-    const text = new THREE.Mesh(textGeometry, textMaterial)
-    text.position.set(-2.1,2.6,-1.8)
-    text.rotateX(-1.6)
-    cube.add(text)
-  })
-
-    week.add(cube) // add cube to scene
-    x--
-  }
-}
-
-function generateWeeks(fontLoader) {
-  let weeks  = []
-  let z = 1
-  while (z > -3) {
-    const week = new THREE.Object3D()
-    generateDays(week, fontLoader)
-    week.position.set(0, 0, z * 7 + 3.5)
-    weeks.push(week)
-    scene.add(week)
-    z--
-  }
-  return weeks
-} 
+Array(100).fill().forEach(addStar) // add 100 stars to scene
 
 const loader = new FontLoader()
-let weeks = generateWeeks(loader) // get weeks objects
-
-function animate() {
-  requestAnimationFrame( animate ) // ask browser to perform animation
-
-  controls.update() // update camera position
-  pointLight.position.set(camera.position.x, camera.position.y, camera.position.z) // move light with camera
-  renderer.render(scene, camera)  // render scene with camera
-}
+let weeks = generateWeeks() // get weeks objects
+generateTaskList() // generate task list
 
 animate()
 
